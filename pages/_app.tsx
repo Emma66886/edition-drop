f65e40808729d5c0411300ff9f8416497e80cf8c
phone Dev <52328102+Emma66886@users.noreply.github.com>
2023-05-25 17:31:04 +0000

    Update 3 files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,21 +5,21 @@ import Head from "next/head";
 import ThirdwebGuideFooter from "../components/GitHubLink";
 
 // This is the chain your dApp will work on.
-const activeChain = "mumbai";
+const activeChain = "base";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider activeChain={""}>
       <Head>
-        <title>thirdweb Edition Drop Minting Customizable Page</title>
+        <title>KINEMPUB| KINEMPUB X BASE JOURNEY NFT</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
-          content="Learn How To Use Thirdweb's Edition Drop contract and create a customizable Edition Drop minting page"
+          content="Mint your KINEMPUB X BASE JOURNEY TO MAINNET"
         />
         <meta
           name="keywords"
-          content="Thirdweb, thirdweb Edition drop, how to make thirdweb nft drop, how to make nft collection thirdweb"
+          content="Kinempub, base, base nft, base token, earning platform for content creators, No.1 Cryptocurrency earning platform"
         />
       </Head>
       <Component {...pageProps} />
